f55fd13dc9c36b9b5a8e942cef9335c5e3c3ea7d
Godwin Pro <maduegodwin2605@gmail.com>
2025-11-06 15:56:52 +0100

    created the server for this project

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import consola from "consola";
+import dotenv from "dotenv";
+import dbConnection from "./connection/dbConnection.js";
+
+const app = express();
+dotenv.config();
+
+app.use(cors());
+app.use(
+    bodyParser.urlencoded({
+        limit: "50",
+        extended: true,
+        parameterLimit: 50000,
+    }),
+    bodyParser.json({ limit: "50" })
+);
+dbConnection();
+app.listen(process.env.PORT || 8081, () => {
+    consola.success({
+        message: `Server started on port ${process.env.PORT || 8081}`,
+        badge: true,
+    });
+});
