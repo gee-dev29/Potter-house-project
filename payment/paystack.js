59be4a6f3184b37e9510ad530d04189e106af662
Godwin Pro <maduegodwin2605@gmail.com>
2025-11-06 17:08:27 +0100

    paystack implementation

---
+++ payment/paystack.js
@@ -0,0 +1,11 @@
+
+import paystack from 'paystack-api';
+
+const initializeTransaction = async (req, res) => {
+  try {
+    const {email , amount} = req.body;
+    const paystack = paystack
+  } catch (error) {
+    
+  }
+}
